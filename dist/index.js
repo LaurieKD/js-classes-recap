52b8a89520e0f5b8611372e4086954e27715e527
LaurieKD <124356856+LaurieKD@users.noreply.github.com>
2023-04-26 15:31:59 +0200

    Setters, getters & super()

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 class Player {
+	static description = "Player in the game";
 	#score = 0;
 	numLives = 10;
 	constructor(first, last) {
@@ -13,6 +14,14 @@ class Player {
 	get score() {
 		return this.#score;
 	}
+
+	set score(newScore) {
+		if (newScore < 0) {
+			throw new Error("Lowest possible score is zero");
+		}
+		this.#score = newScore;
+	}
+
 	getScore() {
 		return this.#score;
 	}
@@ -27,19 +36,29 @@ class Player {
 	}
 }
 
-const player1 = new Player("Pietje", "Puk");
-// player1.taunt();
-// console.log(player1.first);
-// console.log(player1.last);
-// console.log(player1);
-console.log(player1.numLives);
-player1.loseLife();
-console.log(player1.numLives);
-console.log(player1.getScore());
-player1.updateScore(10);
-console.log(player1.getScore());
-console.log(player1.fullName);
-console.log(player1.score);
+class AdminPlayer extends Player {
+	constructor(first, last, privileges) {
+		super(first, last);
+		this.privileges = privileges;
+	}
+	isAdmin = true;
+}
+const admin = new AdminPlayer("John", "Adminson", ["delete players", "add players", "create new world"]);
 
-const player2 = new Player("Charlie", "Brown");
-player2.taunt();
+// const player1 = new Player("Pietje", "Puk");
+// // player1.taunt();
+// // console.log(player1.first);
+// // console.log(player1.last);
+// // console.log(player1);
+// console.log(player1.numLives);
+// player1.loseLife();
+// console.log(player1.numLives);
+// console.log(player1.getScore());
+// player1.updateScore(10);
+// console.log(player1.getScore());
+// console.log(player1.fullName);
+// console.log(player1.score);
+// player1.score = 345;
+
+// const player2 = new Player("Charlie", "Brown");
+// player2.taunt();
